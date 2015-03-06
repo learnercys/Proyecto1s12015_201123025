8b3c7102c7f758d9df93656b48f1ba745d07fccd
Carlos Hernández <learner.cys@gmail.com>
2015-03-05 21:08:49 -0600

    Creations.
    add ignore files.
    create main application 'app'
    add static files.

--- users_system/users_system/settings.py
+++ users_system/users_system/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+    'djangular'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +82,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static")
+)
