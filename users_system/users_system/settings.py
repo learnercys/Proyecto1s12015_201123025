f57417b9332f9db9c9494afe68db7ec3b7ebc54b
Carlos Hernández <learner.cys@gmail.com>
2015-03-06 00:46:06 -0600

    create index page section.

--- users_system/users_system/settings.py
+++ users_system/users_system/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangular'
+    'djangular',
+    'app'
 )
 
 MIDDLEWARE_CLASSES = (
--- users_system/views/home.py
+++ users_system/views/home.py
@@ -7,4 +7,4 @@ class HomeView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
-        return super(HomeView,self).get_context_data(**kwargs)
+        return super(HomeView, self).get_context_data(**kwargs)
